42eb2b68687839492360e49b10230bf884082229
Thanh Le <mihthanh27@gmail.com>
2022-06-19 21:32:20 +0700

    Change unit

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -21,7 +21,7 @@ export function Balance() {
           d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
         />
       </svg>
-      <span>{data ? `BNB ${data.formatted}` : ""}</span>
+      <span>{data ? `MATIC ${data.formatted}` : ""}</span>
     </div>
   );
 }
